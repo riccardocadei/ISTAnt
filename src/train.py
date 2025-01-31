03d75c1553e0854e2f904e349662a1c0fba2080a
riccardocadei <riccardo.cadei98@gmail.com>
2025-01-31 08:15:57 +0100

    add derm

--- src/train.py
+++ src/train.py
@@ -4,19 +4,45 @@ from copy import deepcopy
 from model import MLP
 import numpy as np
 
-
-def train_(supervised, batch_size=1024, num_epochs=20, hidden_nodes = 512, hidden_layers = 1, lr=0.0001, verbose=True):
+def train_(supervised, batch_size=1024, num_epochs=20, hidden_nodes = 512, hidden_layers = 1, lr=0.0001, verbose=True, decondounded=False):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #device = torch.device("cpu")
     if verbose: print(f"Device: {device}")
-
-    # get training and validation set
+    N = supervised["T"].shape[0]
     X = supervised["X"]
     y = supervised["Y"]
+    E = supervised["E"]
+    var_map = {} # Var(Y|E=e)/Var(Y)
+    var = y.var()
+    for e in np.unique(E):
+        mask = E==e
+        if mask.sum() <= 1:
+            var_map[e] = 0
+        else:
+            var_map[e] = y[mask].var()/var
+    Yvars = torch.tensor([var_map[e_i] for e_i in E.numpy()])
+    prob_map = {} # P(Y,E)
+    
+    for e in np.unique(E):
+        mask = E==e
+        if mask.sum() == 0:
+            prob_map[e]
+        else:
+            y1 = (y[mask]==1).sum()/N if (y[mask]==1).sum()/N>0.001 else 0.001
+            y0 = (y[mask]==0).sum()/N if (y[mask]==0).sum()/N>0.001 else 0.001
+            if y.task=="or":
+                prob_map[e] = [y0, y1]
+            elif y.task=="sum":
+                y2 = (y[mask]==2).sum()/N if (y[mask]==2).sum()/N>0.001 else 0.001
+                prob_map[e] = [y0, y1, y2]
+    Oprobs = torch.tensor([prob_map[e_i][int(y_i)] for e_i,y_i in zip(E.numpy(),y.numpy())])
+    weights = Yvars / Oprobs
+
     split = supervised["split"]
     X_train, y_train = X[split], y[split]
     X_val, y_val = X[~split], y[~split]
     y_train.task, y_val.task = y.task, y.task
-    train_loader = DataLoader(TensorDataset(X_train, y_train), batch_size=batch_size, shuffle=True)
+    train_loader = DataLoader(TensorDataset(X_train, y_train, weights[split]), batch_size=batch_size, shuffle=True)
 
     # get model
     input_size = X.shape[1]
@@ -26,32 +52,39 @@ def train_(supervised, batch_size=1024, num_epochs=20, hidden_nodes = 512, hidde
     model.token = X.token
     model.encoder = X.encoder_name
     if y.task == "sum":
-        loss_fn = torch.nn.CrossEntropyLoss()
+        if decondounded:
+            loss_fn = torch.nn.CrossEntropyLoss(reduction='none')
+        else:
+            loss_fn = torch.nn.CrossEntropyLoss()
     else:
         pos_weight = ((y_train==0).sum(dim=0)/(y_train==1).sum(dim=0)).to(device)
-        loss_fn = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+        if decondounded:
+            loss_fn = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight, reduction='none')
+        else:
+            loss_fn = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     if verbose: print("Starting perfomances")
-    train_accs, train_precisions, train_recalls = evaluate_model(model, X_train, y_train, device)
-    if verbose: print_performances(train_accs, train_precisions, train_recalls, y.task, environment="train")
-    val_accs, val_precisions, val_recalls = evaluate_model(model, X_val, y_val, device)
-    if verbose: print_performances(val_accs, val_precisions, val_recalls, y.task, environment="val")
+    train_accs, train_baccs, train_precisions, train_recalls = evaluate_model(model, X_train, y_train, device)
+    if verbose: print_performances(train_accs, train_baccs, train_precisions, train_recalls, y.task, environment="train")
+    val_accs, val_baccs, val_precisions, val_recalls = evaluate_model(model, X_val, y_val, device)
+    if verbose: print_performances(val_accs, val_baccs, val_precisions, val_recalls, y.task, environment="val")
 
     best_val_bacc = 0
     train_metrics = []
     val_metrics = []
     for epoch in range(1, num_epochs+1):
-        if verbose: print(f"Epoch {epoch}")
         model.train()
         train_loss = 0
-        for X_batch, y_batch in train_loader:
+        for X_batch, y_batch, weight in train_loader:
             X_batch, y_batch = X_batch.to(device), y_batch.to(device)
             if y.task=="sum":
                 y_batch = y_batch.long()
             optimizer.zero_grad()
             y_pred = model(X_batch).squeeze()
             loss = loss_fn(y_pred, y_batch) 
+            if decondounded:
+                loss = (loss * weight.to(device)).sum()
             loss.backward()
             optimizer.step()
             train_loss += loss.item()
@@ -70,6 +103,10 @@ def train_(supervised, batch_size=1024, num_epochs=20, hidden_nodes = 512, hidde
         train_metrics.append([train_accs, train_baccs, train_precisions, train_recalls])
         val_metrics.append([val_accs, val_baccs, val_precisions, val_recalls])
 
+    if best_val_bacc==0:
+        best_model = deepcopy(model)
+        best_model.best_epoch = epoch   
+
     best_model.train_metrics = train_metrics
     best_model.val_metrics = val_metrics
 
@@ -77,6 +114,7 @@ def train_(supervised, batch_size=1024, num_epochs=20, hidden_nodes = 512, hidde
 
 def train_md(supervised, batch_size=1024, num_epochs=20, hidden_nodes = 256, hidden_layers = 1, lr=0.0001, verbose=True, ic_weight=1):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #device = torch.device("cpu")
     if verbose: print(f"Device: {device}")
 
     # get training and validation set
@@ -98,18 +136,22 @@ def train_md(supervised, batch_size=1024, num_epochs=20, hidden_nodes = 256, hid
     y_train.task = y.task
 
     val_loaders = []
-    X_val, y_val = X[E==envs_val[0]], y[E==envs_val[0]]
-    val_loaders.append(DataLoader(TensorDataset(X_val, y_val), batch_size=batch_size, shuffle=True))
-    for env in envs_val[1:]:
-        X_val_i, y_val_i = X[E==env], y[E==env]
-        val_loaders.append(DataLoader(TensorDataset(X_val_i, y_val_i), batch_size=batch_size, shuffle=True))
-        X_val = torch.cat((X_val, X_val_i), dim=0)
-        y_val = torch.cat((y_val, y_val_i), dim=0)
-    y_val.task = y.task
+    if len(envs_val)!=0:
+        X_val, y_val = X[E==envs_val[0]], y[E==envs_val[0]]
+        val_loaders.append(DataLoader(TensorDataset(X_val, y_val), batch_size=batch_size, shuffle=True))
+        for env in envs_val[1:]:
+            X_val_i, y_val_i = X[E==env], y[E==env]
+            val_loaders.append(DataLoader(TensorDataset(X_val_i, y_val_i), batch_size=batch_size, shuffle=True))
+            X_val = torch.cat((X_val, X_val_i), dim=0)
+            y_val = torch.cat((y_val, y_val_i), dim=0)
+        y_val.task = y.task
+    else:
+        X_val, y_val = X_train[:0], y_train[:0]
+        y_val.task = y.task
 
     # get model
     input_size = X.shape[1]
-    model = MLP(input_size, hidden_nodes=hidden_nodes, hidden_layers=hidden_layers, task="rep").to(device)
+    model = MLP(input_size, hidden_nodes=hidden_nodes, hidden_layers=hidden_layers, task=y.task).to(device)
     model.device = device
     model.task = y.task
     model.token = X.token
@@ -147,6 +189,8 @@ def train_md(supervised, batch_size=1024, num_epochs=20, hidden_nodes = 256, hid
                     X_batch, y_batch = next(train_loader_iter)
                 except StopIteration:
                     raise RuntimeError()
+                if y.task=="sum":
+                    y_batch = y_batch.long()
                 X_batch, y_batch = X_batch.to(device), y_batch.to(device)
                 optimizer.zero_grad()
                 y_pred = model(X_batch).squeeze()
@@ -171,6 +215,10 @@ def train_md(supervised, batch_size=1024, num_epochs=20, hidden_nodes = 256, hid
         train_metrics.append([train_accs, train_baccs, train_precisions, train_recalls])
         val_metrics.append([val_accs, val_baccs, val_precisions, val_recalls])
 
+    if best_val_bacc==0:
+        best_model = deepcopy(model)
+        best_model.best_epoch = epoch  
+    
     best_model.train_metrics = train_metrics
     best_model.val_metrics = val_metrics
     
